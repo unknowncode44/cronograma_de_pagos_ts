60c5c57238d8895e1fa1139c467af92e20dd602b
HVDevs <80708560+unknowncode44@users.noreply.github.com>
2026-04-29 00:02:10 -0300

    feat: setup tailwind v4 con vite plugin

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
 <template>
   <h1>You did it!</h1>
-  <p>
+  <p class="text-red-900 text-xl">
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
