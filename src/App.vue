64885d7d847332b145935ac3ed28f3b7bd24c2d6
HVDevs <80708560+unknowncode44@users.noreply.github.com>
2026-04-29 00:38:19 -0300

    creada variable reactiva, tipo y lista de dias

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// importamos ref que nos permitira crear variables reactivas
+import { ref } from 'vue';
+
+// tipo personalizado para los dias hábiles de la semana
+type DiaLaboral = 'Lunes' | 'Martes' | 'Miércoles' | 'Jueves' | 'Viernes';
+
+// creamos una lista de DiaLaboral
+const dias: DiaLaboral[] = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes'];
+
+// creamos una variable reactiva que contendra el día seleccionado cuando toquemos el boton del día
+// aseguramos que esta variable solo reciba elementos del tipo DiaLaboral (por ejemplo: ref<DiaLaboral>('Lunes'))
+const diaSeleccionado = ref<DiaLaboral>('Lunes')
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p class="text-red-900 text-xl">
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <!-- html va aca -->
 </template>
 
-<style scoped></style>
+<style scoped>
+/* estilos van aca */
+</style>
